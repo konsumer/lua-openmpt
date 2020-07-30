c4538840c66b79ded0f8686412a022a949074785
David Konsumer <konsumer@jetboystudio.com>
2020-07-30 00:14:29 -0700

    got lua working

--- openmpt-interface.lua
+++ openmpt-interface.lua
@@ -1,17 +1,1 @@
 local openmpt = ffi.load("openmpt")
-
-function Openmpt (filename)
-  local f = io.open (filename, 'r')
-  contents = f:read()
-  f:close()
-
-  o = {
-    filename = filename,
-    module = openmpt.openmpt_module_create_from_memory2(contents, #contents, nil, nil, nil)
-  }
-  setmetatable(o, self)
-  self.__index = self
-  return o
-end
-
-return Openmpt
--- test.lua
+++ test.lua
@@ -1,3 +1,9 @@
-local Openmpt = require "openmpt"
+require "openmpt"
 
-local mod = Openmpt("./plainsong.xm")
+local f = io.open ("./plainsong.xm", 'r')
+contents = f:read("*all")
+f:close()
+local module = openmpt.openmpt_module_create_from_memory2(contents, #contents, nil, nil, nil, nil, nil, nil, nil)
+
+print("speed", openmpt.openmpt_module_get_current_speed(module))
+print("tempo", openmpt.openmpt_module_get_current_tempo(module))
