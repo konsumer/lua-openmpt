7cfab064b7dbf033c9082ab842e8be1819f28db5
David Konsumer <konsumer@jetboystudio.com>
2020-08-08 17:55:45 -0700

    plays but stutters

--- README.md
+++ README.md
@@ -4,8 +4,19 @@ This is a lua wrapper for [libopenmpt](https://lib.openmpt.org/libopenmpt/). It 
 
 ## usage
 
+### linux
+
 On Ubuntu 20.04, I ran `apt install luajit libopenmpt-dev`.
+
+### mac
+
+I ran `brew install luajit libopenmpt` and for love-demos, needed to unsign the love-runtime:
+
+```
+codesign --remove-signature /Applications/love.app/Contents/MacOS/love
+```
+
 
 Run `luajit demo_info.lua` to test it out.
 
-Copy [openmpt.lua](openmpt.lua) into your project, and have a look at `demo_*` files for an idea of how to use it. `demo_love` is a player exmaple.
+Copy [openmpt.lua](openmpt.lua) into your project, and have a look at `demo_*` files for an idea of how to use it. `demo_love` is a player example.
--- demo_love/main.lua
+++ demo_love/main.lua
@@ -13,23 +13,22 @@ dspTime = 0.0
 local mod = OpenMPT:new("./plainsong.xm")
 
 function love.load()
-    sd = love.sound.newSoundData(bufferSize, samplingRate, bitDepth, channelCount)
-    qs = love.audio.newQueueableSource(samplingRate, bitDepth, channelCount)
+  sd = love.sound.newSoundData(bufferSize, samplingRate, bitDepth, channelCount)
+  qs = love.audio.newQueueableSource(samplingRate, bitDepth, channelCount)
 end
 
 function love.update(dt)
-     if qs:getFreeBufferCount() == 0 then return end
-     local samplesToMix = bufferSize
-     for smp = 0, samplesToMix-1 do
-         mod:read_mono(samplingRate, smp, sd:getFFIPointer())
-
-         pointer = pointer + 1
-         dspTime = dspTime + (1 / samplingRate)
-         if pointer >= sd:getSampleCount() then
-            mod:set_position_seconds(dspTime)
-             pointer = 0
-             qs:queue(sd)
-             qs:play()
-         end
-     end
+  if qs:getFreeBufferCount() == 0 then return end
+  local samplesToMix = bufferSize
+  mod:read_mono(samplingRate, bufferSize, sd:getFFIPointer())
+  for smp = 0, samplesToMix-1 do
+    pointer = pointer + 1
+    dspTime = dspTime + (1 / samplingRate)
+    if pointer >= sd:getSampleCount() then
+      mod:set_position_seconds(dspTime)
+      pointer = 0
+      qs:queue(sd)
+      qs:play()
+    end
+  end
 end
