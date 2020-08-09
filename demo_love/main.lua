253c5d83b9757256cea599e66a425478908bf4fd
David Konsumer <konsumer@jetboystudio.com>
2020-08-08 22:37:38 -0700

    cleanup

--- demo_love/main.lua
+++ demo_love/main.lua
@@ -8,13 +8,9 @@ local samplingRate = 44100
 local channelCount = 2
 local bufferSize = 1024
 local pointer = 0
-local currentBpm = 0
 local modChannelCount = 0
 local modInstCount = 0
-local modSpeed = 0
-local modTempo = 0
 local modLength = 0
-local modPatternNum = 0
 
 local mod
 
@@ -31,10 +27,6 @@ function love.update(dt)
   if qs:getFreeBufferCount() == 0 then return end
   local samplesToMix = bufferSize
   mod:read_interleaved_stereo(samplingRate, bufferSize, sd:getFFIPointer())
-  currentBpm = mod:get_current_estimated_bpm()
-  modSpeed = mod:get_current_speed()
-  modTempo = mod:get_current_tempo()
-  modPatternNum = mod:get_current_pattern()
   for smp = 0, samplesToMix-1 do
     pointer = pointer + 1
     if pointer >= sd:getSampleCount() then
@@ -46,13 +38,13 @@ function love.update(dt)
 end
 
 function love.draw()
-  love.graphics.print( "Current BPM: " .. currentBpm, 10,10 )
-  love.graphics.print( "Speed: " .. modSpeed, 10,25 )
-  love.graphics.print( "Tempo: " .. modTempo, 10,40 )
-  love.graphics.print( "Current Pattern: " .. modPatternNum, 10,55 )
+  love.graphics.print( "Current BPM: " .. mod:get_current_estimated_bpm(), 10,10 )
+  love.graphics.print( "Speed: " .. mod:get_current_speed(), 10,25 )
+  love.graphics.print( "Tempo: " .. mod:get_current_tempo(), 10,40 )
+  love.graphics.print( "Current Pattern: " .. mod:get_current_pattern(), 10,55 )
   love.graphics.print( "Channels: " .. modChannelCount, 10,70 )
   love.graphics.print( "Instruments: " .. modInstCount, 10,85 )
-  love.graphics.print( "Song Length (s): " .. modLength, 10,100 )
+  love.graphics.print( "Position (s): " .. mod:get_position_seconds() .. " / " .. modLength, 10,100 )
 
   for channel=0,(modChannelCount - 1) do
     love.graphics.print( string.format(" %02d", channel+1), 10 + (channel*30), 300 )
